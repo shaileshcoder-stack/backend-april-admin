90b320511c5b8ff402fb4a5272e8607881b0b64c
shailesh-co <infopsatech@gmail.com>
2025-04-10 12:41:45 +0530

    "changes added"

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 
+
 dotenv.config();
 const app = express();
 
@@ -10,7 +11,7 @@ connectDB(); // MongoDB connect
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/api/admin', require('./routes/admin.routes'));
 // Routes
 app.use('/api/auth', require('./routes/auth.routes'));
 
